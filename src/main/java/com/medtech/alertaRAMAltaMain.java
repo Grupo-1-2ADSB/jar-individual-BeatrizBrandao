13b7535c7dc19c02100e7aeb8f41c42c2cbb56ff
BeatrizGBrandao <beatriz.brandao@sptech.school>
2024-05-02 23:02:07 -0300

    Adicionei classes para alertas quando a RAM estiver alta

---
+++ src/main/java/com/medtech/alertaRAMAltaMain.java
@@ -0,0 +1,28 @@
+package com.medtech;
+
+import com.github.britooo.looca.api.group.memoria.Memoria;
+import com.medtech.model.alertas.NotificacaoRAM;
+import com.medtech.model.componente.memoria.MonitoramentoMemoria;
+
+public class alertaRAMAltaMain {
+    private static final double VALOR_MEMORIA_ALTA = 0.8;
+
+    public static void main(String[] args) {
+        MonitoramentoMemoria mon1 = new MonitoramentoMemoria();
+
+        Memoria memoria = mon1.exibeMemoria();
+
+        double limiteMemoriaAlta = memoria.getTotal() * VALOR_MEMORIA_ALTA;
+
+        if (memoria.getEmUso() > limiteMemoriaAlta) {
+            double porcentagemEmUso = (double) memoria.getEmUso() / memoria.getTotal() * 100;
+
+            System.out.println("ALERTA: Uso de memória alto!");
+            System.out.printf("Porcentagem de memória em uso: %.2f%%\n", porcentagemEmUso);
+
+            NotificacaoRAM.exibirNotificacao();
+        } else {
+            System.out.println("Uso de memória dentro do limite.");
+        }
+    }
+}
---
+++ src/main/java/com/medtech/model/alertas/NotificacaoRAM.java
@@ -0,0 +1,79 @@
+package com.medtech.model.alertas;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.net.URI;
+
+public class NotificacaoRAM {
+    public static void exibirNotificacao() {
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+
+            SystemTray tray = SystemTray.getSystemTray();
+
+            Image image = Toolkit.getDefaultToolkit().createImage("C:\\Users\\Beatriz Brandão\\OneDrive - SPTech School\\PI\\medTech-jar-individual\\jar-individual-BeatrizBrandao\\src\\main\\java\\org\\example\\logoMedTech.png");
+
+            TrayIcon trayIcon = new TrayIcon(image, "MedTech");
+            trayIcon.setImageAutoSize(true);
+            trayIcon.setToolTip("MedTech");
+
+            tray.add(trayIcon);
+
+            trayIcon.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    JFrame frame = new JFrame("Uso Alto da Memória - Confirmação para fechar processos");
+                    frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                    frame.setSize(500, 170);
+                    frame.setLocationRelativeTo(null);
+
+                    JButton button1 = new JButton("Ok");
+                    JButton button2 = new JButton("Negar");
+
+                    button1.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            System.out.println("Fechar processos!");
+                            // Chamar função para fechar processos
+                            System.exit(0);
+                        }
+                    });
+
+                    button2.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            System.out.println("Processo interrompido! Usuário negou confirmação.");
+                            System.exit(0);
+                        }
+                    });
+
+
+                    JLabel label = new JLabel("<html><div style='padding: 20px 15px 0 15px; text-align: justify;'>Devido à alta ocupação da memória do computador, precisaremos fechar alguns programas para melhorar o desempenho. Não se preocupe, isso não afetará o seu trabalho. Mas, primeiro precisamos da sua confirmação:</div></html>");
+
+                    frame.add(label, BorderLayout.NORTH);
+
+                    JPanel panel = new JPanel();
+                    panel.add(button1);
+                    panel.add(button2);
+                    frame.add(panel);
+
+                    frame.addWindowListener(new WindowAdapter() {
+                        @Override
+                        public void windowClosing(WindowEvent e) {
+                            System.out.println("Processo interrompido! Janela fechada pelo usuário.");
+                            System.exit(0);
+                        }
+                    });
+
+                    frame.setVisible(true);
+                }
+            });
+
+            trayIcon.displayMessage("Devido à alta ocupação da memória, ", "precisaremos fechar alguns programas para melhorar o desempenho. Não se preocupe, isso não afetará o seu trabalho.", TrayIcon.MessageType.WARNING);
+        } catch (Exception ex) {
+            System.err.println("Erro ao criar a notificação: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+}
